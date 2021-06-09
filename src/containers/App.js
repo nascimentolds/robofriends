82af156d0ea6a9fd529a9f3b313306302e962b26
nascimentolds <47907106+nascimentolds@users.noreply.github.com>
2021-06-09 01:18:59 -0300

    Final

--- src/containers/App.js
+++ src/containers/App.js
@@ -31,8 +31,7 @@ class App extends Component {
       return robot.name.toLowerCase().includes(searchfield.toLowerCase());
     })
 
-    return !robots.length ? 
-      <h1>Loading...</h1> :
+    return !robots.length ? <h1>Loading...</h1> :
       (
         <div className='tc'>
           <h1 className='f1'>Robofriends</h1>
