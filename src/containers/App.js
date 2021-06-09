d8fd4bf4ab793f144c3ff4a73891e0ef56fa785a
nascimentolds <47907106+nascimentolds@users.noreply.github.com>
2021-06-09 02:41:43 -0300

    Page link

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
+import ErrorBoundry from '../components/ErrorBoundry';
 //import { robots } from './robots';
 import './App.css';
 
@@ -37,7 +38,9 @@ class App extends Component {
           <h1 className='f1'>Robofriends</h1>
           <SearchBox searchChange={this.onSearchChange} />
           <Scroll>
-            <CardList robots={filterdRobots} />
+            <ErrorBoundry>
+              <CardList robots={filterdRobots} />
+            </ErrorBoundry>
           </Scroll>
         </div>
       );
